a84ed5db40e32a299a201b9a828cfc67c4aa093f
Christian Mocanita <mocanitachristian1234@gmail.com>
2026-03-25 13:07:50 +0200

    feat: Am adaugat functia handleDelete si am adaugat butonul Sterge langa fiecare task adaugat

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -7,6 +7,11 @@ function TodoList() {
  setTodos([...todos, input]); // Creeaza array NOU cu tot ce era + input
  setInput(''); // Goleste input-ul
  }
+ function handleDelete(index) {
+ setTodos(todos.filter(function(_, i) {
+ return i !== index;
+ }));
+ }
  return (
  <div>
  <h3>Todo List</h3>
@@ -17,9 +22,16 @@ function TodoList() {
  />
  <button onClick={handleAdd}>Adauga</button>
  <ul>
-    {todos.map(function(todo, index){
-        return <li key={index}>{todo}</li>
+ <ul>
+    {todos.map(function(todo, index) {
+        return (
+            <li key={index}>
+                {todo}
+                <button onClick={() => handleDelete(index)}>Sterge</button>
+            </li>
+        );
     })}
+</ul>
  </ul>
  </div>
  );
