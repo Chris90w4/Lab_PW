8a82009906a83a818dfcbab479af56b350e9a080
Christian Mocanita <mocanitachristian1234@gmail.com>
2026-03-25 12:43:13 +0200

    feat: Am adaugat QuickNote cu un input controlat

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import Card from './Card';
+import QuickNote from './QuickNote';
 
 const projects = [
-        { title: "Proiect 1", description: "Pagina personala" },
-        { title: "Proiect 2", description: "Calculator buget" },
-        { title: "Proiect 3", description: "Dashboard React" },
-    ];
+    { title: "Proiect 1", description: "Pagina personala" },
+    { title: "Proiect 2", description: "Calculator buget" },
+    { title: "Proiect 3", description: "Dashboard React" },
+];
 
 function App() {
     return (
         <div>
             <h1>Dashboard</h1>
-             {projects.map(function(item, index) {
+            <QuickNote />
+            {projects.map(function(item, index) {
                 return (
-                    <Card 
+                    <Card
                         key={index}
                         title={item.title}
                         description={item.description}
@@ -21,9 +23,6 @@ function App() {
             })}
         </div>
     );
-    
 }
-
-
 
 export default App;
