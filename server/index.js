5f091adfca278405043241eeccad802399754912
Christian Mocanita <mocanitachristian1234@gmail.com>
2026-05-18 09:07:11 +0300

    Rescriere ruta GET pentru a citi din MongoDB

--- server/index.js
+++ server/index.js
@@ -10,8 +10,7 @@ console.log('Conectat la MongoDB!');
 .catch(function(err) { 
 console.error('Eroare conectare MongoDB:', err); 
 }); 
-
-
+const Project = require('./models/Project');
 const PORT = 3000;
 
 app.use(cors());
@@ -24,35 +23,40 @@ app.get('/', function(req, res) {
 
 
 // Date (temporar in memorie, vom folosi MongoDB mai tarziu)
-const projects = [
- { id: 1, title: "Pagina Personala", tech: "HTML, CSS", done: true },
- { id: 2, title: "Calculator Buget", tech: "JS", done: true },
- { id: 3, title: "Dashboard React", tech: "React", done: false },
- { id: 4, title: "API Meteo", tech: "React, API", done: false },
-];
+// const projects = [
+//  { id: 1, title: "Pagina Personala", tech: "HTML, CSS", done: true },
+//  { id: 2, title: "Calculator Buget", tech: "JS", done: true },
+//  { id: 3, title: "Dashboard React", tech: "React", done: false },
+//  { id: 4, title: "API Meteo", tech: "React, API", done: false },
+// ];
 // GET /api/projects - returneaza toate proiectele
-app.get('/api/projects', function(req, res) {
- res.json(projects);
+app.get('/api/projects', async function(req, res) {
+    try {
+        const projects = await Project.find();
+        res.json(projects);
+    } catch (err) {
+        res.status(500).json({ error: 'Eroare ' + err });
+    }
 });
 
 
-app.get('/api/projects/:id', function(req, res) {
- const id = parseInt(req.params.id);
- const project = projects.find(p => p.id === id);
- if (!project) {
-     return res.status(404).json({ error: 'Not found' });
- }
- res.json(project);
-});
+// app.get('/api/projects/:id', function(req, res) {
+//  const id = parseInt(req.params.id);
+//  const project = projects.find(p => p.id === id);
+//  if (!project) {
+//      return res.status(404).json({ error: 'Not found' });
+//  }
+//  res.json(project);
+// });
 
-app.get('/api/stats', function(req, res) {
- const stats = {
-     total: projects.length,
-     finalizate: projects.filter(p => p.done).length,
-     inLucru: projects.filter(p => !p.done).length
- };
- res.json(stats);
-});
+// app.get('/api/stats', function(req, res) {
+//  const stats = {
+//      total: projects.length,
+//      finalizate: projects.filter(p => p.done).length,
+//      inLucru: projects.filter(p => !p.done).length
+//  };
+//  res.json(stats);
+// });
 
 // POST /api/projects - adauga un proiect nou
 app.post('/api/projects', function(req, res) {
